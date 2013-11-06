2e5f798a15f8043896ad399aac683d01f6cf30dc
David Whitney <david@davidwhitney.co.uk>
2013-11-06 17:53:47 +0000

    Added ConfigurationManagerTests

---
+++ System.Configuration.Abstractions.Test.Unit/ConfigurationManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace System.Configuration.Abstractions.Test.Unit
+{
+    [TestFixture]
+    public class ConfigurationManagerTests
+    {
+        #pragma warning disable 618
+        [Test]
+        public void Instance_IsNotNull_IsSingleton()
+        {
+            var instance1 = ConfigurationManager.Instance;
+            var instance2 = ConfigurationManager.Instance;
+
+            Assert.That(instance1, Is.EqualTo(instance2));
+        }
+        #pragma warning restore 618
+    }
+}
