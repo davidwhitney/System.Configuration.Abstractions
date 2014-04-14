27e542411e0844926b1d2769b188978063039dbc
David Whitney <david@davidwhitney.co.uk>
2014-04-14 15:57:56 +0100

    Added Map<T> to the IAppSettingsExtended interface. Moved Raw to IAppSettingsExtended from IAppSettings (IAppSettings inherits from IAppSettingsExtended so this is a non-breaking change). Added release notes.

--- System.Configuration.Abstractions/IAppSettingsExtended.cs
+++ System.Configuration.Abstractions/IAppSettingsExtended.cs
@@ -1,8 +1,13 @@
-﻿namespace System.Configuration.Abstractions
+﻿using System.Collections.Specialized;
+
+namespace System.Configuration.Abstractions
 {
     public interface IAppSettingsExtended
     {
+        NameValueCollection Raw { get; }
+
         string AppSetting(string key, Func<string> whenKeyNotFoundInsteadOfThrowingDefaultException = null);
         T AppSetting<T>(string key, Func<T> whenKeyNotFoundInsteadOfThrowingDefaultException = null);
+        TSettingsDto Map<TSettingsDto>() where TSettingsDto : class, new();
     }
 }
