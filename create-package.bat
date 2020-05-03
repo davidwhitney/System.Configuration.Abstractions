83537539a50aa3bfc1a7e24a557d62dfde97adf9
David Whitney <david@davidwhitney.co.uk>
2020-05-03 14:06:02 +0100

    Migrating to nuget actions.

--- create-package.bat
+++ create-package.bat
@@ -1,5 +1,2 @@
-call build.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 .nuget\nuget pack "System.Configuration.Abstractions\System.Configuration.Abstractions.csproj" -Properties Configuration=Release
 if %errorlevel% neq 0 exit /b %errorlevel%
